3430e9c2d3354d697c01e3309ec30d9d397e11c3
KalsonSaint <saintkalson@gmail.com>
2020-09-17 04:20:12 +0100

    Routing Complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch, Redirect } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Movies from "./components/Movies";
+import MovieForm from './components/movieForm'
 import Customers from "./components/Customers";
 import Rentals from "./components/Rentals";
 import NotFound from "./components/NotFound";
@@ -16,7 +17,9 @@ class App extends Component{
         <Navbar />
         <main className="container">
           <Switch>
-            <Route exact path="/movies" component={Movies} />
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
             <Route path="/customers" component={Customers} />
             <Route exact path="/rentals" component={Rentals} />
             <Route path="/not-found" component={NotFound} />
--- src/components/movieForm.js
+++ src/components/movieForm.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-function movieForm() {
+function movieForm({match, history}) {
     return (
         <div>
-            <h1>Movie Form</h1>
+            <h1>Movie Form {match.params.id}</h1>
+            <button className="btn btn-primary" onClick={()=> history.push('/movies')}>Go Back</button>
         </div>
     )
 }
